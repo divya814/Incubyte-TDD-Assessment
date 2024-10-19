22cc044618e01f7aa25afa40d0cc005d9b5bda19
Divya Maheshwari <divya.maheshwari814@gmail.com>
2024-10-19 16:58:46 +0530

    Added file for test cases

---
+++ test_string_calculator.py
@@ -0,0 +1,16 @@
+import unittest
+from string_calculator import add
+
+class TestStringCalculator(unittest.TestCase):
+
+    def test_empty_string(self):
+        self.assertEqual(add(""), 0)
+
+    def test_single_number(self):
+        self.assertEqual(add("1"), 1)
+
+    def test_two_numbers(self):
+        self.assertEqual(add("1,5"), 6)
+
+if __name__ == '__main__':
+    unittest.main()
